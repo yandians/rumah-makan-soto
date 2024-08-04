f59f29546194135b99088ee9f8f973043cd7e777
Asep <asepsuherman389@gmail.com>
2024-08-05 04:15:47 +0700

    Update dari seno

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call([ProduksSeeder::class]);
         $this->call([MakanansSeeder::class]);
         $this->call([UsersTableSeeder::class]);
         $this->call([KasSeeder::class]);
